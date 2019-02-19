21ca85e0980a9b293f28b2e768949633c5b501bf
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2019-02-19 14:03:36 -0700

    changes

--- assignment3/color_detection.py
+++ assignment3/color_detection.py
@@ -13,28 +13,38 @@ def updateText(pic, red, green, blue):
     green = "Green:" + str(green)
     blue = "Blue:" + str(blue)
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(pic, red, (10, 50), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
-    cv2.putText(pic, green, (10, 75), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
-    cv2.putText(pic, blue, (10, 100), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(pic, red, (10, 50), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(pic, green, (10, 75), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(pic, blue, (10, 100), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
 def main():
 
-    picture_1 = cv2.imread("candy1.jpg", cv2.IMREAD_COLOR)
-    picture_2 = cv2.imread('candy2.jpg', cv2.IMREAD_COLOR)
-    cv2.namedWindow("Candy", cv2.WINDOW_KEEPRATIO)
+    picture_1 = cv2.imread("imagesWOvideo/one.jpg", cv2.IMREAD_COLOR)
+    picture_2 = cv2.imread("imagesWOvideo/two.jpg", cv2.IMREAD_COLOR)
+    picture_3 = cv2.imread("imagesWOvideo/three.jpg", cv2.IMREAD_COLOR)
+    picture_4 = cv2.imread("imagesWOvideo/four.jpg", cv2.IMREAD_COLOR)
+
+    cv2.namedWindow("Candy1", cv2.WINDOW_KEEPRATIO)
     cv2.namedWindow("Candy2", cv2.WINDOW_KEEPRATIO)
-    cv2.moveWindow("Candy", 0, 0)
-    cv2.moveWindow("Candy2", 600, 0)
+    cv2.namedWindow("Candy3", cv2.WINDOW_KEEPRATIO)
+    cv2.namedWindow("Candy4", cv2.WINDOW_KEEPRATIO)
+    cv2.moveWindow("Candy1", 0, 0)
+    cv2.moveWindow("Candy2", 410, 0)
+    cv2.moveWindow("Candy3", 820, 0)
+    cv2.moveWindow("Candy4", 0, 350)
     red = 0
     green = 0
     blue = 0
     #initialize counts to 0
     updateText(picture_1, red, green, blue)
     updateText(picture_2, red, green, blue)
+    updateText(picture_3, red, green, blue)
+    updateText(picture_4, red, green, blue)
 
-
-    cv2.imshow("Candy", picture_1)
+    cv2.imshow("Candy1", picture_1)
     cv2.imshow("Candy2", picture_2)
+    cv2.imshow("Candy3", picture_2)
+    cv2.imshow("Candy4", picture_2)
 
 
 
