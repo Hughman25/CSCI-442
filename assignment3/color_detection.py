5fc9fab6622c981b34ec440cd9382faaf59ccbc7
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2019-02-19 10:30:23 -0700

    changes

--- assignment3/color_detection.py
+++ assignment3/color_detection.py
@@ -4,13 +4,13 @@ import numpy as np
 
 
 def main():
-    picture_1 = cv2.imread('candy1.jpg',0) #0 loads image in as grayscale
-    picture_2 = cv2.imread('candy2.jpg',0)
+
+    picture_1 = cv2.imread('candy1.jpg') # ,0 loads image in as grayscale
+    picture_2 = cv2.imread('candy2.jpg')
     cv2.imshow("Candy", picture_1)
     cv2.imshow("Candy 2", picture_2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
 
 
 
