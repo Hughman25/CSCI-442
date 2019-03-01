b32c44c406b8ca6690400516959e605b0ad4b548
Hughman25 <hughman25@gmail.com>
2019-03-01 15:35:51 -0700

    finalized

--- assignment3/color_detection.py
+++ assignment3/color_detection.py
@@ -30,41 +30,41 @@ def getAverage(pic, x, y):
 def forceColor(red, green, blue):
     #force green
     color = "white"
-    if((red >= 0 and red <= 45) and (green >= 170 and green <= 255) and (blue <= 195 and blue >= 70)):
+    if((red >= 0 and red <= 140) and (green >= 140 and green <= 255) and (blue <= 217 and blue >= 26)):
         red = 49
         green = 172
         blue = 85
         color = "green"
     #force red
-    elif((red <= 255 and red >= 170) and (green <= 140 and green >= 45) and (blue <= 180 and blue >= 60)):
+    elif((red <= 255 and red >= 165) and (green <= 140 and green >= 15) and (blue <= 180 and blue >= 0)):
         red = 177
         green = 18
         blue = 36
         color = "red"
     #force blue
-    elif((red <= 35 and red >= 0) and (green <= 235 and green >= 120) and (blue <= 255 and blue >= 220)):
+    elif((red <= 75 and red >= 0) and (green <= 235 and green >= 120) and (blue <= 255 and blue >= 185)):
         red = 47
         green = 159
         blue = 215
         color = "blue"
+    #force orange
+    elif((red <= 255 and red >= 180) and (green <= 200 and green >= 70) and (blue >= 10 and blue <= 175)):
+       red = 242
+       green = 111
+       blue = 34
+       color = "orange"
     #force yellow
-    elif((red <= 255 and red >= 200) and (green >= 200 and green <= 255) and (blue >= 0 and blue <= 170)):
+    elif((red <= 255 and red >= 200) and (green >= 200 and green <= 255) and (blue >= 0 and blue <= 137)):
         red = 255
         green = 242
         blue = 0
         color = "yellow"
     #force brown
-    elif((red >= 60 and red <= 160) and (green >= 70 and green <= 200) and (blue <= 200 and blue >= 70)):
+    elif((red >= 0 and red <= 140) and (green >= 0 and green <= 160) and (blue <= 175 and blue >= 0)):
         red = 96
         green = 58
         blue = 52
         color = "brown"
-    #force orange
-    elif((red <= 255 and red >= 220) and (green <= 200 and green >= 90) and (blue >= 10 and blue <= 165)):
-       red = 242
-       green = 111
-       blue = 34
-       color = "orange"
     #if we miss the range keep it white
     else:
         red = 255
@@ -92,12 +92,10 @@ def updateText(pic, red, green, blue, yellow, orange, brown):
 
 def main():
     #read the original images into memory
-    picture_1 = cv2.imread("imagesWOvideo/one.jpg", cv2.IMREAD_COLOR)
-    picture_2 = cv2.imread("imagesWOvideo/two.jpg", cv2.IMREAD_COLOR)
-    picture_3 = cv2.imread("imagesWOvideo/three.jpg", cv2.IMREAD_COLOR)
-    picture_4 = cv2.imread("imagesWOvideo/four.jpg", cv2.IMREAD_COLOR)
-    pictures = [picture_1, picture_2, picture_3, picture_4]
-    #res1 = picture_1.copy()
+    picture_1 = cv2.imread("candyBigSmallerTiny.jpg", cv2.IMREAD_COLOR)
+    pictures = [picture_1]#, picture_2, picture_3, picture_4]
+
+    #width, height, _ = picture_1.shape
     width = 600
     height = 800
     #initialize black frames for force color
@@ -105,89 +103,34 @@ def main():
     res1[:, 0:width//2] = (0, 0, 0)      # (B, G, R)
     res1[:, width//2:width] = (0, 0, 0)
 
-    res2 = np.zeros((height, width, 3), np.uint8)
-    res2[:, 0:width//2] = (0, 0, 0)      # (B, G, R)
-    res2[:, width//2:width] = (0, 0, 0)
+    blurs = (cv2.GaussianBlur(picture_1, (5, 5), 1))
 
-    res3 = np.zeros((height, width, 3), np.uint8)
-    res3[:, 0:width//2] = (0, 0, 0)      # (B, G, R)
-    res3[:, width//2:width] = (0, 0, 0)
-
-    res4 = np.zeros((height,width,3), np.uint8)
-    res4[:, 0:width//2] = (0, 0, 0)      # (B, G, R)
-    res4[:, width//2:width] = (0, 0, 0)
-    #blur the images with 5x5
-    blurs = []
-    for i in range(len(pictures)):
-        blurs.append(cv2.GaussianBlur(pictures[i], (5, 5), 1))
-
-    edges = []
+    #edges = []
     #run canny edge detector to detect edges
-    for i in range(len(blurs)):
-        edges.append(cv2.Canny(blurs[i], 200, 150))
+    #for i in range(len(blurs)):
+    edges = (cv2.Canny(blurs, 208, 135))
     #create named windows
-    cv2.namedWindow("Candy1", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Candy2", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Candy3", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Candy4", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Edge1", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Edge2", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Edge3", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Edge4", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Res1", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Res2", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Res3", cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow("Res4", cv2.WINDOW_KEEPRATIO)
+    cv2.namedWindow("Candy1", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Edge1", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Res1", cv2.WINDOW_NORMAL)
 
     #move named windows 
     cv2.moveWindow("Candy1", 0, 0)
-    cv2.moveWindow("Candy2", 410, 0)
-    cv2.moveWindow("Candy3", 820, 0)
-    cv2.moveWindow("Candy4", 0, 350)
-    cv2.moveWindow("Edge1", 410, 350)
-    cv2.moveWindow("Edge2", 820, 350)
-    cv2.moveWindow("Edge3", 900, 350)
-    cv2.moveWindow("Edge4", 0, 350)
+    cv2.moveWindow("Edge1", 400, 0)
+
     #initialize r,g,b,y,o,br values
-
-
-    circle1 = cv2.HoughCircles(edges[0], cv2.HOUGH_GRADIENT, 1, 35, param1=1, param2=38, minRadius=8, maxRadius=0)
-    circle2 = cv2.HoughCircles(edges[1], cv2.HOUGH_GRADIENT, 1, 35, param1=1, param2=35, minRadius=8, maxRadius=0)
-    circle3 = cv2.HoughCircles(edges[2], cv2.HOUGH_GRADIENT, 1, 35, param1=1, param2=35, minRadius=8, maxRadius=0)
-    circle4 = cv2.HoughCircles(edges[3], cv2.HOUGH_GRADIENT, 1, 35, param1=1, param2=55, minRadius=8, maxRadius=0)
-                              
+    circle1 = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 35, param1=1, param2=6, minRadius=8, maxRadius=18)
+    
     circles1 = np.uint16(np.around(circle1))
-    circles2 = np.uint16(np.around(circle2))
-    circles3 = np.uint16(np.around(circle3))
-    circles4 = np.uint16(np.around(circle4))
     #save the y,x locations of the center of a circle
     circleLocations1 = []
-    circleLocations2 = []
-    circleLocations3 = []
-    circleLocations4 = []
     for i in circles1[0, :]:
         # draw the outer circle
-        cv2.circle(edges[0], (i[0], i[1]), i[2] - 5, (255, 255, 255), -1)
+        cv2.circle(edges, (i[0], i[1]), i[2] - 5, (255, 255, 255), -1)
         #save the location of the circles
         circleLocations1.append((i[0],i[1]))
-
-    for i in circles2[0, :]:
-        # draw the outer circle
-        cv2.circle(edges[1], (i[0], i[1]), i[2] - 5, (255, 255, 255), -1)
-        #save the location of the circles
-        circleLocations2.append((i[0],i[1]))
-    for i in circles3[0, :]:
-        # draw the outer circle
-        cv2.circle(edges[2], (i[0], i[1]), i[2] - 5, (255, 255, 255), -1)
-        #save the location of the circles
-        circleLocations3.append((i[0], i[1]))
-    for i in circles4[0, :]:
-        # draw the outer circle
-        cv2.circle(edges[3], (i[0], i[1]), i[2] - 5, (255, 255, 255), -1)
-        #save the location of the circles
-        circleLocations4.append((i[0], i[1]))
-    
     #use the locations to find original color in circle
+
     num_red = 0
     num_green = 0
     num_blue = 0
@@ -220,87 +163,10 @@ def main():
     num_orange = 0
     num_brown = 0
     color = ""
-    for i in range(len(circleLocations2)):
-        rgb = picture_2[circleLocations2[i][1], circleLocations2[i][0]]
-        red, green, blue = getAverage(picture_2, circleLocations2[i][0], circleLocations2[i][1])
-        red, green, blue, color = forceColor(red, green, blue)
-        cv2.circle(res2, (circleLocations2[i][0], circleLocations2[i][1]), 15, (blue, green, red), -1)
-        if(color == "red"):
-            num_red += 1
-        elif (color == "green"):
-            num_green += 1
-        elif (color == "blue"):
-            num_blue += 1
-        elif (color == "yellow"):
-            num_yellow += 1
-        elif (color == "orange"):
-            num_orange += 1
-        elif (color == "brown"):
-            num_brown += 1
-    updateText(picture_2, num_red, num_green, num_blue, num_yellow, num_orange, num_brown)
-    num_red = 0
-    num_green = 0
-    num_blue = 0
-    num_yellow = 0
-    num_orange = 0
-    num_brown = 0
-    color = ""
-    for i in range(len(circleLocations3)):
-        rgb = picture_3[circleLocations3[i][1], circleLocations3[i][0]]
-        red, green, blue = getAverage(picture_3, circleLocations3[i][0], circleLocations3[i][1])
-        red, green, blue, color = forceColor(red, green, blue)
-        cv2.circle(res3, (circleLocations3[i][0], circleLocations3[i][1]), 15, (blue, green, red), -1)
-        if(color == "red"):
-            num_red += 1
-        elif (color == "green"):
-            num_green += 1
-        elif (color == "blue"):
-            num_blue += 1
-        elif (color == "yellow"):
-            num_yellow += 1
-        elif (color == "orange"):
-            num_orange += 1
-        elif (color == "brown"):
-            num_brown += 1
-    updateText(picture_3, num_red, num_green, num_blue, num_yellow, num_orange, num_brown)
-    num_red = 0
-    num_green = 0
-    num_blue = 0
-    num_yellow = 0
-    num_orange = 0
-    num_brown = 0
-    color = ""
-    for i in range(len(circleLocations4)):
-        rgb = picture_4[circleLocations4[i][1], circleLocations4[i][0]]
-        red, green, blue = getAverage(picture_4, circleLocations4[i][0], circleLocations4[i][1])
-        red, green, blue, color = forceColor(red, green, blue)
-        cv2.circle(res4, (circleLocations4[i][0], circleLocations4[i][1]), 15, (blue, green, red), -1)
-        if(color == "red"):
-            num_red += 1
-        elif (color == "green"):
-            num_green += 1
-        elif (color == "blue"):
-            num_blue += 1
-        elif (color == "yellow"):
-            num_yellow += 1
-        elif (color == "orange"):
-            num_orange += 1
-        elif (color == "brown"):
-            num_brown += 1
-    updateText(picture_4, num_red, num_green, num_blue, num_yellow, num_orange, num_brown)
-
+    
     cv2.imshow("Candy1", picture_1)
-    cv2.imshow("Candy2", picture_2)
-    cv2.imshow("Candy3", picture_3)
-    cv2.imshow("Candy4", picture_4)
-    cv2.imshow("Edge1", edges[0])
-    cv2.imshow("Edge2", edges[1])
-    cv2.imshow("Edge3", edges[2])
-    cv2.imshow("Edge4", edges[3])
+    cv2.imshow("Edge1", edges)
     cv2.imshow("Res1", res1)
-    cv2.imshow("Res2", res2)
-    cv2.imshow("Res3", res3)
-    cv2.imshow("Res4", res4)
 
 
     #import video file
