eb968e2d7c4f7af6f7ef2500ffb360a87e6cffe4
Matthew Sagen <matthewsagen@Matthews-MacBook-Pro.local>
2019-03-14 11:32:57 -0600

    changes

--- assignment4/robotCode/pythonFiles/yellow_road.py
+++ assignment4/robotCode/pythonFiles/yellow_road.py
@@ -12,8 +12,8 @@ camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 yellow_color_boundaries = [(0, 145, 190), (137, 255, 255)]
-lower_yellow_bound = np.array(yellow_color_boundaries[0], dtype="uint8")
-upper_yellow_bound = np.array(yellow_color_boundaries[1], dtype="uint8")
+lower_yellow_bound = np.array([25, 50, 50], dtype="uint8")
+upper_yellow_bound = np.array([32, 255, 255], dtype="uint8")
 
 MOTORS = 1
 TURN = 2
@@ -22,7 +22,7 @@ HEADTILT = 4
 HEADTURN = 3
 
 tango = maestro.Controller()
-body = 6000
+body = 5500
 headTurn = 6000
 headTilt = 1600
 turn = 6000
@@ -34,72 +34,87 @@ tango.setTarget(TURN, turn)
 tango.setTarget(BODY, body)
 
 # allow the camera to warmup
-time.sleep(0.1)
+time.sleep(1)
+
 def findCoG(img):
     white_pixels = np.argwhere(img >= 200)
     size = len(white_pixels)
     sumX = 0
     sumY = 0
-    for x, y in white_pixels:
+    for y, x in white_pixels:
         sumX += x
         sumY += y
-    sumX = sumX / size
-    sumY = sumY / size
+    if(size > 0):
+        sumX = sumX / size
+        sumY = sumY / size
+    else:
+        return(-1, -1)
     return (x, y)
 
+def stop():
+    time.sleep(3)
+    motors = 6000
+    turn = 6000
+    tango.setTarget(MOTORS, motors)
+    tango.setTarget(TURN, turn)
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-    blur = cv2.GuassianBlur()
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # new_hsv = hsv
-    # for loop in hsv:
-    #     for loop2 in loop:
-    #         new_hsv = (loop+25) % 180
-    #pic = cv2.Canny(hsv[0], 100, 50)
-    #mask = cv2.inRange(image, lower_yellow_bound, upper_yellow_bound)
+    split_image = image[290:640, 0:480]
+    blur = cv2.GaussianBlur(split_image, (5, 5), 1)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_yellow_bound, upper_yellow_bound)
     #pic = cv2.Canny(mask, 100, 170)
-    pic = cv2.Canny(hsv[0], 100, 170)
+    pic = cv2.Canny(mask, 100, 50)
 
     cog = findCoG(pic)
-    cv2.rectangle(pic, (cog[0]+10, cog[1]+10), (cog[0]-10, cog[1]-10), (255,0,0), 1, 8)
+    if(cog[0] != -1 and cog[1] != -1):
+        cv2.rectangle(pic, (cog[0]+10, cog[1]+10), (cog[0]-10, cog[1]-10), (255, 0, 0), 1, 8)
     # show the frame
     cv2.imshow("Frame", pic)
-    if cog[1] < 160:
+    if cog[1] < 50:
         motors = 6000
         turn = 6000
         tango.setTarget(MOTORS, motors)
         tango.setTarget(TURN, turn)
-        break
+        print("end")
+        #break
     #near the center
     elif 330 < cog[0] > 310:
         #move forward
-        motors = 6600
+        motors = 5350
         tango.setTarget(MOTORS, motors)
+        print("forward")
     elif 400 > cog[0] > 330:
-        #move left slightly
-        turn = 2600
+       #move right slightly
+        turn = 5350
         tango.setTarget(TURN, turn)
+        print("right slightly")
     elif cog[0] > 400:
-        #move left hard
-        turn = 2110
-        tango.setTarget(TURN, turn)
-    elif 260 < cog[0] > 310:
-        #move right slightly
-        turn = 6600
-        tango.setTarget(TURN, turn)
-    elif cog[0] < 260:
         #move right hard
-        turn = 7000
+        turn = 5350
         tango.setTarget(TURN, turn)
+        print("right hard")
+    elif 260 < cog[0] > 310:
+         #move left slightly
+        turn = 6400
+        tango.setTarget(TURN, turn)
+        print("left slightly")
+    elif cog[0] < 260:
+        #move left hard
+        turn = 6650
+        tango.setTarget(TURN, turn)
+        print("left hard")
+
     key = cv2.waitKey(1) & 0xFF
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
-
+    stop()
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
             motors = 6000
