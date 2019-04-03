d01ffbc68f0ead94d3d178681a0ab8be229cd40a
Hughman25 <hughman25@gmail.com>
2019-04-03 13:03:38 -0600

    added methods for robot functions

--- assignment5/pythonFiles/interact.py
+++ assignment5/pythonFiles/interact.py
@@ -38,11 +38,12 @@ tango.setTarget(HEADTILT, headTilt)
 #tango.setTarget(BODY, body)
 
 # allow the camera to warmup
-
+time.sleep(1)
 
 # capture frames from the camera
 cv2.namedWindow("Robo", cv2.WINDOW_NORMAL)
 cv2.moveWindow("Robo", 0, 0)
+
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     
     # grab the raw NumPy array representing the image, then initialize the timestamp
@@ -124,25 +125,106 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         motors = 6000
         tango.setTarget(MOTORS, motors)
        
-    cv2.imshow("Robo", image)
+    showImage()
     
 
     key = cv2.waitKey(1) & 0xFF
-
-
-    #stop()
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
-            #motors = 6000
-            turn = 6000
-            headTilt = 6000
-            #tango.setTarget(MOTORS, motors)
-            tango.setTarget(TURN, turn)
-            tango.setTarget(HEADTILT, headTilt)
-            tango.setTarget(BODY, 6000)
+            shutdown()
             break
 
+def showImage(image):
+        cv2.imshow("Robo", image)
+        key = cv2.waitKey(1) & 0xFF
 
 
+        #stop()
+         # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+
+def shutdown():
+        #motors = 6000
+        turn = 6000
+        headTilt = 6000
+        #tango.setTarget(MOTORS, motors)
+        tango.setTarget(TURN, turn)
+        tango.setTarget(HEADTILT, headTilt)
+        tango.setTarget(BODY, 6000)
+
+def findHuman():
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+                image = frame.array
+                face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
+                faces = face_cascade.detectMultiScale(image, 1.3, 4)
+                if (faces != null):
+                        #send msg hello human
+                        centerFace()
+
+def centerBody():
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+                image = frame.array
+                face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
+                faces = face_cascade.detectMultiScale(image, 1.3, 4)
+                for (x,y,w,h) in faces:
+                        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+                        xcenter = x + int((w/2))
+                        ycenter = y + int((h/2)) 
+                        xdist = 320 - xcenter
+                        ydist = 240 - ycenter
+                        xabs = abs(320 - xcenter)
+                        yabs = abs(240 - ycenter)
+                        
+                        if((xabs > 30) or (yabs > 20)):
+                                if(6000 + (xabs*2) >= 6300):
+                                        if(xdist > 0): #turn robot left 
+                                                if(body < 6000): #if was previously turned other way
+                                                        body = 6000
+                                                if(body == 6000):
+                                                        body = 6600
+                                                elif(body == 6600): #already turned body, so turn machine
+                                                        turn = 7000
+                                                        tango.setTarget(MOTORS, motors)
+                                                        tango.setTarget(TURN, turn)
+                                                        time.sleep(0.5)
+                                                        body = 6000
+                                                tango.setTarget(TURN, 6000)
+                                                tango.setTarget(BODY, body)
+                                        elif(xdist < 0): # turn robot right
+                                                if(body > 6000): # if was previously turned other way
+                                                        body = 6000
+                                                elif(body == 6000):
+                                                        body = 5400
+                                                elif(body == 5400):
+                                                        turn = 5000
+                                                        tango.setTarget(MOTORS, motors)
+                                                        tango.setTarget(TURN, turn)
+                                                        time.sleep(0.5)
+                                                        body = 6000
+                                                tango.setTarget(TURN, 6000)
+                                                tango.setTarget(BODY, body)
+                                
+                                tango.setTarget(HEADTURN, 6000 + (xdist*2))
+                                tango.setTarget(HEADTILT, 6000 + (int(ydist*2.5)))
+                        area = x * y 
+                        print(area)
+                        if(area > 45000): #move forwwards
+                                motors = 5200
+                                tango.setTarget(MOTORS, motors)
+                                time.sleep(0.35)
+                        elif(area < 35000): #move backwards
+                                motors = 6900
+                                tango.setTarget(MOTORS, motors)       
+                                time.sleep(0.35)
+                        else:
+                                motors = 6000
+                                tango.setTarget(MOTORS, motors)  
+                        motors = 6000
+                        tango.setTarget(MOTORS, motors)
+
+def centerScreen():
+        tango.setTarget(HEADTURN, headTurn)
+        tango.setTarget(HEADTILT, headTilt)
+
+def centerDistance():
+        pass
